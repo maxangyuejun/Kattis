f42e81d300a44cc81fee67db1eb5394f40f82913
Max Ang Yue Jun <87632389+slackernoob@users.noreply.github.com>
2022-05-04 16:58:26 +0800

    Stuck In A Time Loop
    Problem ID: timeloop

---
+++ timeloop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class timeloop {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int reps = sc.nextInt();
+		for (int i = 1; i <= reps; i++) {
+			System.out.println(i + " Abracadabra");
+		}
+	}
+}
